46ffd45649cd6d82c5d5c4475a6132257b51fb0f
Lucas Emanoel <lucas2014.barros@gmail.com>
2024-08-21 00:13:24 -0300

    fix facts

--- animalis-factos.pl
+++ animalis-factos.pl
@@ -1,6 +1,8 @@
 % Fatos
 
 % Definição do reino
+reino(animais).
+
 filo_do_reino(cordados, animais).
 filo_do_reino(artropodes, animais).
 filo_do_reino(poriferos, animais).
@@ -18,6 +20,7 @@ classe_do_filo(demosponjas, poriferos).
 % Definição de ordens
 ordem_da_classe(carnivoros, mamiferos).
 ordem_da_classe(artiodactilos, mamiferos).
+ordem_da_classe(perissodactilos, mamiferos).
 ordem_da_classe(cetaceos, mamiferos).
 
 ordem_da_classe(pinguins, aves).
@@ -30,79 +33,57 @@ ordem_da_classe(perciformes, peixes_osseos).
 ordem_da_classe(lepidopteros, insetos).
 
 % Definição de famílias
-familia_da_ordem(felideos, carnivoros).
+familia_da_ordem(felinos, carnivoros).
 familia_da_ordem(canideos, carnivoros).
-familia_da_ordem(elefantideos, artiodactilos).
-familia_da_ordem(giraffideos, artiodactilos).
+
+familia_da_ordem(elefantes, artiodactilos).
+familia_da_ordem(girafas, artiodactilos).
 familia_da_ordem(equideos, artiodactilos).
 
-familia_da_ordem(rinocerotideos, perissodactilos).
-familia_da_ordem(delphinideos, cetaceos).
-familia_da_ordem(balaenopterideos, cetaceos).
+familia_da_ordem(rinocerontes, perissodactilos).
+familia_da_ordem(delfinideos, cetaceos).
+familia_da_ordem(baleias, cetaceos).
 
 familia_da_ordem(spheniscideos, pinguins).
 
-familia_da_ordem(crocodilideos, crocodilianos).
+familia_da_ordem(crocodilos, crocodilianos).
+familia_da_ordem(serpentes, escamados).
+
 familia_da_ordem(pomacentrideos, perciformes).
 
-familia_da_ordem(lepido, lepidopteros).
-familia_da_ordem(spongideos, demosponjas).
-
-% Definição de gêneros e espécies
-genero_da_familia(panthera, felideos).
-genero_da_familia(canis, canideos).
-genero_da_familia(elephas, elefantideos).
-genero_da_familia(giraffa, giraffideos).
-genero_da_familia(equus, equideos).
-
-genero_da_familia(ceratotherium, rinocerotideos).
-genero_da_familia(delphinus, delphinideos).
-genero_da_familia(balaenoptera, balaenopterideos).
-
-genero_da_familia(aptentodytes, spheniscideos).
-
-genero_da_familia(crocodylus, crocodilideos).
-
-genero_da_familia(amphiprion, pomacentrideos).
-
-genero_da_familia(papilio, lepido).
-genero_da_familia(spongia, spongideos).
+familia_da_ordem(borboletas, lepidopteros).
+familia_da_ordem(esponjas, demosponjas).
 
 % Exemplos de espécies
-especie_do_genero(leao, panthera).
-especie_do_genero(tigre, panthera).
-especie_do_genero(gato, panthera).
+especie_da_familia(leao, felinos).
+especie_da_familia(tigre, felinos).
+especie_da_familia(gato, felinos).
 
-especie_do_genero(cao, canis).
+especie_da_familia(cao, canideos).
 
-especie_do_genero(elefante, elephas).
-especie_do_genero(girafa, giraffa).
-especie_do_genero(cavalo, equus).
+especie_da_familia(elefante, elefantes).
+especie_da_familia(girafa, girafas).
+especie_da_familia(cavalo, equideos).
 
-especie_do_genero(rinoceronte, ceratotherium).
-especie_do_genero(golfinho, delphinus).
-especie_do_genero(baleia, balaenoptera).
+especie_da_familia(rinoceronte, rinocerontes).
+especie_da_familia(golfinho, delfinideos).
+especie_da_familia(baleia, baleias).
 
-especie_do_genero(pinguim, aptentodytes).
+especie_da_familia(pinguim, pinguins).
 
-especie_do_genero(crocodilo, crocodylus).
-especie_do_genero(jacare, crocodylus).
+especie_da_familia(crocodilo, crocodilianos).
+especie_da_familia(jacare, crocodilianos).
 
-especie_do_genero(peixe_palhaco, amphiprion).
+especie_da_familia(cobra, serpentes).
 
-especie_do_genero(borboleta, papilio).
+especie_da_familia(peixe_palhaco, perciformes).
 
-especie_do_genero(esponja, spongia).
+especie_da_familia(borboleta, borboletas).
 
-% Regras para consultas
-animal(X) :- filo_do_reino(_, X).
-filo(X) :- classe_do_filo(_, X).
-classe(X) :- ordem_da_classe(_, X).
-ordem(X) :- familia_da_ordem(_, X).
-familia(X) :- genero_da_familia(_, X).
-genero(X) :- especie_do_genero(_, X).
+especie_da_familia(esponja, esponjas).
 
+% Cada animal
 
-
+% Cada nivel
 
 
